d9fa05304c7fe217a53a6fafc1696a8e5c6a6806
sakalakis <32517254+sakalakis@users.noreply.github.com>
2018-12-11 01:36:48 +0200

    Update script1a.sh

--- script1a.sh
+++ script1a.sh
@@ -13,7 +13,19 @@ while IFS=' ' read
 do
         if grep -Fq "$REPLY" $HOME/database.txt
         then
-
+                #calculate the new md5 sum and store to the swap file
+                var2="curl -s $REPLY|md5sum|cut -d ' ' -f 1"
+                eval $var2 >md5swap.dat
+                link=$(head -1 md5swap.dat)
+                while IFS=" " read -r website1 hash1
+                do
+                        if ["$REPLY"="$website1"] && ["$hash1" != "$link"]
+                        then
+                                echo "$REPLY"
+                                #replace the previous hash with the new one
+                                sed -i -e 's/"$hash1"/"$link/g' database.txt
+                        fi
+                done < database.txt
         else
                 echo "$REPLY INIT"
                 var2="curl -s $REPLY|md5sum|cut -d ' ' -f 1"
